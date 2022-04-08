4c4fe192c2b40a647bfd3638766e60888abe5854
Datbui123 <89052320+Datbui123@users.noreply.github.com>
2022-04-08 13:31:41 +0700

    1

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').oneclick = () =>{
+    navbar.classList.toggle('active');
+}
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').oneclick = () =>{
+    navbar.classList.toggle('active');
+}
